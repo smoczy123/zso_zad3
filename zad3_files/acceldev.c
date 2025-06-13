9f7bc1254febc4db453479d39cc4dc5ce2bd4944
smoczy123 <michal.hudobski@poczta.onet.pl>
2025-06-13 21:20:20 +0200

    add create buffer

--- zad3_files/acceldev.c
+++ zad3_files/acceldev.c
@@ -1,4 +1,9 @@
+#include "asm-generic/fcntl.h"
+#include "asm/page.h"
 #include "linux/dma-mapping.h"
+#include "linux/fs.h"
+#include "linux/mm.h"
+#include "linux/mm_types.h"
 #include "linux/slab.h"
 #include "linux/types.h"
 #include <linux/module.h>
@@ -19,7 +24,9 @@
 
 MODULE_LICENSE("GPL");
 
-
+static uint32_t dma_handle_to_page_entry(dma_addr_t dma_handle) {
+	return (uint32_t)(((dma_handle >> ACCELDEV_PAGE_SHIFT) << 4) | 1);
+}
 
 
 struct acceldev_device {
@@ -36,7 +43,81 @@ struct acceldev_device {
 
 struct acceldev_context {
     struct acceldev_device *dev;
+	int buffers[ACCELDEV_NUM_BUFFERS];
 };
+
+struct acceldev_buffer_data {
+	void* page_table_cpu;
+	dma_addr_t page_table_dma;
+	size_t buffer_size;
+	dma_addr_t* pages_dma;
+	void** pages_cpu;
+	struct pci_dev *pdev; 	
+};
+
+static void dealloc_page_table(struct acceldev_buffer_data *buf_data, struct pci_dev *pdev) {
+	int i = 0;
+	if (buf_data->page_table_cpu) {
+		uint32_t *page_table = (uint32_t *)buf_data->page_table_cpu;
+		for (i = 0; i < 1024; i++) {
+   			if (page_table[i] && 1) {
+				dma_free_coherent(&(pdev->dev), ACCELDEV_PAGE_SIZE, buf_data->pages_cpu[i], buf_data->pages_dma[i]);
+   			} else {
+				break;
+			}
+  		}
+	  	dma_free_coherent(&(pdev->dev), 1024 * 32, buf_data->page_table_cpu, buf_data->page_table_dma);		
+	}
+}
+
+static int alloc_page_table(struct acceldev_buffer_data *buf_data, struct pci_dev *pdev) {
+	int err;
+	if ((err = dma_set_mask_and_coherent(&(pdev->dev), DMA_BIT_MASK(64))))
+		goto out_mask;
+	buf_data->page_table_cpu = dma_alloc_coherent(&(pdev->dev), 1024 * 32, &buf_data->page_table_dma, GFP_KERNEL);
+	uint32_t *page_table = (uint32_t *)buf_data->page_table_cpu;
+	memset(buf_data->page_table_cpu, 0, 1024 * 32);
+	if (!buf_data->page_table_cpu) {
+  		err = -ENOMEM;
+  		goto out_alloc;
+ 	}
+	int page_count = buf_data->buffer_size / ACCELDEV_PAGE_SIZE;
+	page_count += (buf_data->buffer_size % ACCELDEV_PAGE_SIZE) ? 1 : 0;
+	buf_data->pages_dma = kmalloc(sizeof(dma_addr_t) * page_count, GFP_KERNEL);
+	if (!buf_data->pages_dma) {
+	 	err = -ENOMEM;
+		goto out_alloc;
+ 	}	
+	buf_data->pages_cpu = kmalloc(sizeof(void*) * page_count, GFP_KERNEL);
+	if (!buf_data->pages_cpu) {
+   		err = -ENOMEM;
+   		goto out_alloc;
+  	}	
+	unsigned long allocated = 0;
+	if ((err = dma_set_mask_and_coherent(&(pdev->dev), DMA_BIT_MASK(40))))
+		goto out_alloc;
+	for (int i = 0; i < page_count; i++) {
+		buf_data->pages_cpu[i] = dma_alloc_coherent(&(pdev->dev), ACCELDEV_PAGE_SIZE, &buf_data->pages_dma[i], GFP_KERNEL);
+		if (!buf_data->pages_cpu[i]) {
+			err = -ENOMEM;
+			goto out_alloc;
+		}
+		memset(buf_data->pages_cpu[i], 0, ACCELDEV_PAGE_SIZE);
+		allocated += ACCELDEV_PAGE_SIZE;
+		page_table[i] = dma_handle_to_page_entry(buf_data->pages_dma[i]);
+	}
+	dma_set_mask_and_coherent(&(pdev->dev), DMA_BIT_MASK(64));
+
+	return 0;
+
+
+out_alloc:
+ 	dealloc_page_table(buf_data, pdev);
+out_mask:		
+	dma_set_mask_and_coherent(&(pdev->dev), DMA_BIT_MASK(64));
+	return err;
+
+}
 
 
 
@@ -70,7 +151,146 @@ static irqreturn_t acceldev_isr(int irq, void *opaque)
 	return IRQ_RETVAL(0);
 }
 
+
+
+static vm_fault_t buffer_fault(struct vm_fault *vmf) {
+	struct acceldev_buffer_data* buf = (struct acceldev_buffer_data*)(vmf->vma->vm_private_data);
+	unsigned long offset = vmf->pgoff << ACCELDEV_PAGE_SHIFT;
+	if (offset >= buf->buffer_size) {
+		return VM_FAULT_SIGBUS; // Out of bounds access
+	}
+	unsigned long page_index = offset / ACCELDEV_PAGE_SIZE;
+	void* page_addr = buf->pages_cpu[page_index];
+	struct page* page = virt_to_page(page_addr);
+	if (!page) 
+  		return VM_FAULT_SIGBUS; // Page not found
+	get_page(page);
+	vmf->page = page;
+	return 0;
+}
+
+static const struct vm_operations_struct buffer_vm_ops = {
+ .fault = buffer_fault,
+};
+
+static int buffer_release(struct inode *inode, struct file *filp) {
+	// TODO: WAIT FOR CONTEXT TO FINISH
+ 	struct acceldev_buffer_data* buf_data = (struct acceldev_buffer_data*)filp->private_data;
+ 	struct pci_dev *pdev = buf_data->pdev;
+	if (buf_data) {
+  		dealloc_page_table(buf_data, pdev); // Assuming pdev is not needed here
+  		kfree(buf_data->pages_dma);
+  		kfree(buf_data->pages_cpu);
+  		kfree(buf_data);
+ 	}
+ 	return 0;
+}
+
+static int buffer_mmap(struct file *filp, struct vm_area_struct *vma) {
+	
+	vma->vm_private_data = filp->private_data;
+	vma->vm_ops = &buffer_vm_ops;
+	return 0;
+}
+
+static const struct file_operations acceldev_buffer_fops = {
+ .owner = THIS_MODULE,
+ .mmap = buffer_mmap,
+ .release = buffer_release,
+};
+
+static int create_buffer(int size, struct pci_dev *pdev, enum acceldev_buffer_type type, struct acceldev_context *ctx, struct acceldev_ioctl_create_buffer_result *result) {
+	int err;
+	struct acceldev_buffer_data *buf_data = kmalloc(sizeof(struct acceldev_buffer_data), GFP_KERNEL);
+	if (!buf_data) {
+  		return -ENOMEM;
+ 	}
+	buf_data->buffer_size = size;
+	buf_data->pdev = pdev;
+	if ((err = alloc_page_table(buf_data, pdev)) < 0) {
+		kfree(buf_data);
+		return err;
+	}
+
+	int bfd = anon_inode_getfd(ACCELDEV_NAME, &acceldev_buffer_fops, buf_data, O_RDWR);
+	if (bfd < 0) {
+  		dealloc_page_table(buf_data, pdev);
+  		kfree(buf_data);
+ 	}
+	if (type == BUFFER_TYPE_DATA) {
+		for (int i = 0; i < ACCELDEV_NUM_BUFFERS; i++) {
+			if (ctx->buffers[i] == 0) {
+				ctx->buffers[i] = bfd;
+				result->buffer_slot = i;
+				break;
+			}
+		}
+	}
+	return bfd;
+
+}
+
 /* Main device node handling.  */
+
+static long acceldev_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
+	switch (cmd) {
+		case ACCELDEV_IOCTL_CREATE_BUFFER: {
+			struct acceldev_ioctl_create_buffer* create_buf = (struct acceldev_ioctl_create_buffer*)arg;
+			if (!create_buf || 
+				create_buf->size <= 0 || 
+				create_buf->size > ACCELDEV_BUFFER_MAX_SIZE || 
+				create_buf->type >= BUFFER_TYPES_COUNT) {
+				return -EINVAL; // Invalid arguments
+			}
+			struct acceldev_context *ctx = file->private_data;
+			if (create_buf->type == BUFFER_TYPE_DATA) {
+				int i;
+				for (i = 0; i < ACCELDEV_NUM_BUFFERS; i++) {
+					if (ctx->buffers[i] == 0) {
+						break;
+					}
+				}
+				if (i == ACCELDEV_NUM_BUFFERS) {
+					return -ENOSPC;
+				}
+			}
+
+			int size = create_buf->size;
+			enum acceldev_buffer_type type = create_buf->type;
+			int bfd = create_buffer(size, ctx->dev->pdev, type, ctx, create_buf->result);
+
+			return bfd;
+		}
+
+		case ACCELDEV_IOCTL_RUN: {
+			struct acceldev_ioctl_run* run_cmd = (struct acceldev_ioctl_run*)arg;
+			if (!run_cmd ||
+				run_cmd->cfd < 0 || 
+				run_cmd->addr < 0 || 
+				run_cmd->size <= 0 || 
+				run_cmd->addr > ACCELDEV_BUFFER_MAX_SIZE ||
+				run_cmd->size > ACCELDEV_BUFFER_MAX_SIZE - run_cmd->addr || 
+				run_cmd->addr % ACCELDEV_USER_CMD_WORDS * sizeof(uint32_t) != 0 ||
+				run_cmd->size % ACCELDEV_USER_CMD_WORDS * sizeof(uint32_t) != 0) {
+
+				return -EINVAL; // Invalid arguments
+			}
+
+
+			return 0;
+		}
+
+		case ACCELDEV_IOCTL_WAIT: {
+
+
+
+			return 0;
+		}
+		
+	}
+
+	return -ENOTTY; // Not a valid ioctl command
+}
 
 static int acceldev_open(struct inode *inode, struct file *file)
 {
@@ -112,6 +332,8 @@ static const struct file_operations acceldev_file_ops = {
 	.owner = THIS_MODULE,
 	.open = acceldev_open,
 	.release = acceldev_release,
+	.unlocked_ioctl = acceldev_ioctl,
+	.compat_ioctl = acceldev_ioctl,
 }; 
 
 
